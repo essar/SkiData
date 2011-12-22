d2a76b498de4254c60dfc0f592eed1243fdeedab
Steve Roberts <steve.roberts@essarsoftware.co.uk>
2011-12-22 01:09:10 +0000

    Added getSkiAppData method to SkiDataActivity.
    Allowing access to AppData singleton class via parent Android Activity class. May allow more dynamic generalised access to saved data - from memory, from file, from database etc.

--- src/uk/co/essarsoftware/ski/android/ui/SkiDataActivity.java
+++ src/uk/co/essarsoftware/ski/android/ui/SkiDataActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public abstract class SkiDataActivity extends Activity
 {
+	protected AppData getSkiAppData() {
+		return AppData.getAppData();
+	}
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
--- src/uk/co/essarsoftware/ski/android/ui/XYPlotActivity.java
+++ src/uk/co/essarsoftware/ski/android/ui/XYPlotActivity.java
@@ -20,22 +20,33 @@ public class XYPlotActivity extends SkiDataActivity
 {
 	private XYPlotView view;
 	
+	/**
+	 * Redraw the plot using an <tt>AltitudeRenderer</tt> to highlight changes in altitude.
+	 */
 	private void highlightAltitude() {
-		Track t = AppData.getAppData().track;
-		if(t != null) {
+		Track t = getSkiAppData().track;
+		if(t != null && view != null) {
 			AltitudeRenderer ar = new AltitudeRenderer(t);
 			view.setRenderer(ar, true);
 		}
 	}
 	
+	/**
+	 * Redraw the plot using a <tt>ModeRenderer</tt> to highlight changes in skiing mode.
+	 */
 	private void highlightMode() {
-		ModeRenderer mr = new ModeRenderer();
-		view.setRenderer(mr, true);
+		if(view != null) {
+			ModeRenderer mr = new ModeRenderer();
+			view.setRenderer(mr, true);
+		}
 	}
 	
+	/**
+	 * Redraw the plot using a <tt>SpeedRenderer</tt> to highlight changes in speed.
+	 */
 	private void highlightSpeed() {
-		Track t = AppData.getAppData().track;
-		if(t != null) {
+		Track t = getSkiAppData().track;
+		if(t != null && view != null) {
 			SpeedRenderer sr = new SpeedRenderer(t);
 			view.setRenderer(sr, true);
 		}
@@ -50,11 +61,14 @@ public class XYPlotActivity extends SkiDataActivity
 
         // Get plot data from singleton data structure
         // TODO more elegant way of doing this?
-        XYPlot plot = AppData.getAppData().plot;
+        XYPlot plot = getSkiAppData().plot;
         
         if(plot != null) {
-	        // Add view objects
-	        view = new XYPlotView(this, plot, new ModeRenderer());
+        	// Create view object if not already set
+        	if(view == null) {
+        		view = new XYPlotView(this, plot, new ModeRenderer());
+        	}
+	        // Add view object
 	        setContentView(view);
         } else {
         	Log.w(getLocalClassName(), "Plot data is null, unable to draw");
@@ -70,6 +84,19 @@ public class XYPlotActivity extends SkiDataActivity
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.xyplotmenu, menu);
         return true;
+    }
+    
+    /* (non-Javadoc)
+     * @see android.app.Activity#onStop()
+     */
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	
+    	// Clear view object to prevent stale display
+    	view = null;
+    	
+    	Log.d(getLocalClassName(), "XYPlotActivity stopped");
     }
     
     /* (non-Javadoc)
